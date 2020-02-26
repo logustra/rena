1829bdcf150238ca37c5f6b6f4b39e12a861b586
logustra <logustra@gmail.com>
2020-02-26 13:49:44 +0700

    feat: react native with typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function App () {
+  return (
+    <React.Fragment>
+      <Text>Hellow World</Text>
+    </React.Fragment>
+  )
+}
