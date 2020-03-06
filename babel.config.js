cabcf56a640da89e34cfe350ac2e6b53e68d6308
logustra <logustra@gmail.com>
2020-03-07 03:29:40 +0700

    build(babel): update config

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,24 @@ module.exports = {
   presets: [
     'module:metro-react-native-babel-preset',
     'module:react-native-dotenv',
+
+    [
+      '@babel/preset-env', {
+        'useBuiltIns': 'entry',
+        'corejs': 3,
+        'modules': false,
+        'targets': {
+          'node': 'current',
+          'browsers': '> 5%'
+        }
+      }
+    ],
+    
     '@babel/preset-typescript'
   ],
 
   plugins: [
+    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-object-rest-spread',
     [
       '@babel/plugin-proposal-decorators', {
