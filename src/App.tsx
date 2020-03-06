38fcc90b06d51fd893745a1c42d0b25d7066804d
logustra <logustra@gmail.com>
2020-03-06 17:14:05 +0700

    feat: react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import 'react-native-gesture-handler'
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function Home ({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+      <Button
+        title="Go to Blog"
+        onPress={() => navigation.navigate('Blog')}
+      />
+    </View>
+  )
+}
+
+function Blog ({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Blog</Text>
+      <Button 
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  )
+}
 
 export default function App () {
   return (
-    <React.Fragment>
-      <Text>Hellow World</Text>
-    </React.Fragment>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Blog" component={Blog} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
