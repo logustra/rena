2e369f738151d16b13d0c9d7cdfb3cdb57e3f530
logustra <logustra@gmail.com>
2020-03-20 21:51:24 +0700

    improvement(reactotron): remove log

--- src/plugins/reactotron.tsx
+++ src/plugins/reactotron.tsx
@@ -17,8 +17,4 @@ if (__DEV__) {
 
   // Let's clear Reactotron on every time we load the app
   Reactotron.clear()
-
-  // Totally hacky, but this allows you to not both importing reactotron-react-native
-  // on every file.  This is just DEV mode, so no big deal.
-  console.tron = Reactotron
 }
