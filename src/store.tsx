75f191a9466509b3fe51247a8de33522c77455b2
logustra <logustra@gmail.com>
2020-03-28 17:08:24 +0700

    improvement(store): change initial context & add typings

--- src/store.tsx
+++ src/store.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
+import { Props } from '@/contracts/storeContracts'
 import {
   commonInitState,
   commonReducer
 } from '@/stores'
 
-export const Stores = React.createContext(null)
+export const Stores = React.createContext({})
 
-export default function Store ({ children }: any) {
+export default function Store ({ children }: Props) {
   const [commonState, commonDispatch] = React.useReducer(commonReducer, commonInitState)
 
   return (
