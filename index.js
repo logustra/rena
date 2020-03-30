dd062ee10ca048d502f071826ef05fdc94476e28
logustra <logustra@gmail.com>
2020-03-30 21:13:21 +0700

    refactor: rename file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import './src/plugins/reactotron'
 import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+
+import './src/plugins/reactotron'
 
 AppRegistry.registerComponent(appName, () => App)
