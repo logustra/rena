ff56014b3f657cc02b40b393267ada94343e7a30
logustra <logustra@gmail.com>
2020-04-14 15:14:56 +0700

    build(eslint): update

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,37 +53,6 @@ module.exports = {
       'SwitchCase': 1
     }],
 
-    // react
-    'react/prop-types': 'off',
-    'react/jsx-equals-spacing': ['error', 'never'],
-    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
-    'react/jsx-first-prop-new-line': ['error', 'multiline'],
-    'react/jsx-fragments': ['error', 'element'],
-
-    'react/jsx-tag-spacing': ['error', {
-      'beforeSelfClosing': 'always'
-    }],
-
-    'react/jsx-max-props-per-line': ['error', {
-      'maximum': 1
-    }],
-
-    'react/jsx-curly-spacing': ['error', {
-      'when': 'never', 
-      'children': {
-        'when': 'never'
-      }
-    }],
-
-    'react/self-closing-comp': ['error', {
-      'component': true,
-      'html': true
-    }],
-
-    // react-hooks
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-
     // typescript
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
@@ -124,12 +93,43 @@ module.exports = {
     '@typescript-eslint/type-annotation-spacing': ['error', {
       'before': false,
       'after': true,
-      overrides: { 
-        arrow: { 
-          before: true, 
-          after: true 
-        } 
+      overrides: {
+        arrow: {
+          before: true,
+          after: true
+        }
       }
     }],
+
+    // react
+    'react/prop-types': 'off',
+    'react/jsx-equals-spacing': ['error', 'never'],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
+    'react/jsx-fragments': ['error', 'element'],
+
+    'react/jsx-tag-spacing': ['error', {
+      'beforeSelfClosing': 'always'
+    }],
+
+    'react/jsx-max-props-per-line': ['error', {
+      'maximum': 1
+    }],
+
+    'react/jsx-curly-spacing': ['error', {
+      'when': 'never', 
+      'children': {
+        'when': 'never'
+      }
+    }],
+
+    'react/self-closing-comp': ['error', {
+      'component': true,
+      'html': true
+    }],
+
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   }
 }
