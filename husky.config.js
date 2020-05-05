1391c7a7e7af60dcf85061e2dcc24188e8092676
logustra <logustra@gmail.com>
2020-05-05 11:57:00 +0700

    build(husky): update config

--- husky.config.js
+++ husky.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   hooks: {
+    'pre-commit': 'yarn lint',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
